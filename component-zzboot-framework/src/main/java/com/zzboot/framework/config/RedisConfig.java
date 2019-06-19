73a319ad957b5e3aab5f88a2d7fbd1a9ae6e82fe
zhonglh <493843721@qq.com>
2019-06-19 18:17:27 +0800

    项目初始化

--- component-zzboot-framework/src/main/java/com/zzboot/framework/config/RedisConfig.java
+++ component-zzboot-framework/src/main/java/com/zzboot/framework/config/RedisConfig.java
@@ -15,9 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -53,22 +51,74 @@ public class RedisConfig extends CachingConfigurerSupport {
 		return template;
 	}
 
+	/*
 	@Bean
 	public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 		RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder
 				.fromConnectionFactory(redisConnectionFactory);
 		return builder.build();
 	}
+	*/
+
+
+	/**
+	 * 对hash类型的数据操作
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForHash();
+	}
+
+	/**
+	 * 对redis字符串类型数据操作
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public ValueOperations<String, Object> valueOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForValue();
+	}
+
+	/**
+	 * 对链表类型的数据操作
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public ListOperations<String, Object> listOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForList();
+	}
+
+	/**
+	 * 对无序集合类型的数据操作
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public SetOperations<String, Object> setOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForSet();
+	}
+
+	/**
+	 * 对有序集合类型的数据操作
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+		return redisTemplate.opsForZSet();
+	}
 
 
 
-	// 缓存管理器
-	/*@Bean
-	public CacheManager cacheManager(@SuppressWarnings("rawtypes") RedisTemplate redisTemplate) {
-		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-		// 设置缓存过期时间
-		cacheManager.setDefaultExpiration(10000);
-		return cacheManager;
-	}*/
+
+
 
 }
